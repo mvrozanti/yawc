b553fad70b04d19da8fc8f1e7dbe7176337cdca6
Marcelo <mvrozanti@hotmail.com>
2018-10-12 19:47:07 -0300

    added README

---
+++ README.md
@@ -0,0 +1,55 @@
+# Yet Another WhatsApp Client
+
+## Why another one?
+
+- None of them can do automation at the moment
+- Hard to setup
+- Unintuitive commands
+
+## Requirements
+
+- Firefox version 61.0
+
+- Python 3
+
+- GeckoDriver 
+  - `sudo pacman -S geckodriver` on Arch Linux
+
+## Installation
+
+- This project uses [WhatsApp-Wrapper](https://github.com/mukulhase/WebWhatsapp-Wrapper) API so it needs to be cloned *recursively*:
+
+```
+git clone --recursive https://github.com/mvrozanti/yawc \
+  cd yawc/WebWhatsapp-Wrapper \
+  pip3 install --user -r requirements.txt \
+  pip3 install --user .
+```
+
+- Make sure you have geckodriver and Firefox 61 installed:
+  - `geckodriver -v; firefox -v`
+
+## Usage
+
+```
+
+usage: whatsapp.py [-h] [-c] [-C] [-m] [-s MESSAGE] [-B MESSAGE] [-b MESSAGE]
+                   -p LOCATION [-t [CONTACT|CHAT]]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c, --chats           show chats
+  -C, --contacts        show all contacts ever seen by this account
+  -m, --my-contacts     show my contacts (added to address book)
+  -s MESSAGE, --send MESSAGE
+                        send message (requires --to flag)
+  -B MESSAGE, --broadcast MESSAGE
+                        send message to all chats
+  -b MESSAGE, --broadcast-contacts MESSAGE
+                        send message to all contacts
+  -p LOCATION, --profile LOCATION
+                        use firefox profile contained in LOCATION
+  -t [CONTACT|CHAT], --to [CONTACT|CHAT]
+                        apply command only to this CONTACT or CHAT
+
+```
--- whatsapp.py
+++ whatsapp.py
@@ -13,10 +13,6 @@ import sqlite3
 import code
 
 def main():
-    print('Starting driver...')
-    DRIVER = WhatsAPIDriver(profile='/home/nexor/.mozilla/firefox/794uxbrx.default')
-    print('Logging in...')
-    DRIVER.wait_for_login()
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--chats',                action='store_true',        help='show chats')
     parser.add_argument('-C', '--contacts',             action='store_true',        help='show all contacts ever seen by this account')
@@ -24,8 +20,13 @@ def main():
     parser.add_argument('-s', '--send',                 metavar='MESSAGE',          help='send message (requires --to flag)')
     parser.add_argument('-B', '--broadcast',            metavar='MESSAGE',          help='send message to all chats')
     parser.add_argument('-b', '--broadcast-contacts',   metavar='MESSAGE',          help='send message to all contacts')
+    parser.add_argument('-p', '--profile',              metavar='LOCATION',         help='use firefox profile contained in LOCATION', required=True)
     parser.add_argument('-t', '--to',                   metavar='[CONTACT|CHAT]',   help='apply command only to this CONTACT or CHAT', required='-s' in sys.argv)
     args = parser.parse_args()
+    print('Starting driver...')
+    DRIVER = WhatsAPIDriver(profile='/home/nexor/.mozilla/firefox/794uxbrx.default')
+    print('Logging in...')
+    DRIVER.wait_for_login()
     code.interact(local=locals())
     if args.chats: print(json.dumps({'chats' : [c.name for c in DRIVER.get_all_chats()]}, indent=4))
     elif args.my_contacts: print(json.dumps({'my_contacts': [c.name for c in DRIVER.get_my_contacts()]}, indent=4))
